d250930cfc0e5ed070a87f64da6a8444fbbd6fd2
kanzaki <city8i7@yahoo.co.jp>
2022-02-25 00:28:59 +0900

    追記

--- path.py
+++ path.py
@@ -25,3 +25,32 @@ path = 'C:\\Windows\\System32\\calc.exe'
 print(os.path.basename(path))
 print(os.path.dirname(path))
 
+print(os.path.getsize('C:\\Windows\\System32\\calc.exe'))
+#print(os.listdir('C:\\Windows\\System32'))
+
+total_size = 0
+for filename in os.listdir('C:\\Windows\\System32'):
+    total_size = total_size + os.path.getsize(os.path.join('C:\\Windows\\System32', filename))
+
+print(total_size)
+
+print(os.path.exists('C:\\Windows'))
+print(os.path.exists('C:\\some_made_up_floder'))
+print(os.path.isdir('C:\\Windows\\System32'))
+print(os.path.isfile('C:\\Windows\\System32'))
+print(os.path.isdir('C:\\Windows\\System32\\calc.exe'))
+print(os.path.isfile('C:\\Windows\\System32\\calc.exe'))
+print(os.path.exists('D:\\'))
+
+#hello_file = open('C:\\github_pyhon\\python_study\\hello.txt')
+#hello_content = hello_file.read()
+
+bacon_file = open('C:\\github_pyhon\\python_study\\bacon.txt', 'w')
+bacon_file.write('Hello world\n')
+bacon_file.close()
+bacon_file = open('C:\\github_pyhon\\python_study\\bacon.txt', 'a')
+bacon_file.write('Bancon is not a vegetable.')
+bacon_file.close()
+bacon_file =open('C:\\github_pyhon\\python_study\\bacon.txt')
+content = bacon_file.read()
+print(content)
